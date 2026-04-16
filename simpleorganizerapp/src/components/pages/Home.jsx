b385d12ed93209c5b1b5dd046131a7ef66528fd3
Diogo Soares Germano <diogogermano28@gmail.com>
2026-04-15 21:51:49 -0300

    centralize logo

--- simpleorganizerapp/src/components/pages/Home.jsx
+++ simpleorganizerapp/src/components/pages/Home.jsx
@@ -2,7 +2,7 @@ import logo from "../../assets/img/logo-v1.png"
 import styles from "./Home.module.css"
 function Home(){
     return(
-        <div>
+        <div className={styles.homeContainer}>
             <img src={logo} className={styles.logo}></img>
         </div>
     );
